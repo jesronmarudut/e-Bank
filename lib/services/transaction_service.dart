7554c77e315445056f6b25c03a82c55fcd4d1389
jesronmarudut23@gmail.com <jesronmarudut23@gmail.com>
2023-02-20 17:37:58 +0700

    Add BloC for Paket Data

--- lib/services/transaction_service.dart
+++ lib/services/transaction_service.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'package:bank/models/data_plan_form_model.dart';
 import 'package:bank/models/topup_form_model.dart';
 import 'package:bank/models/transfer_form_model.dart';
 import 'package:bank/services/auth_service.dart';
@@ -34,6 +34,26 @@ class TransactionService {
       final token = await AuthService().getToken();
       final res = await http.post(
         Uri.parse('$baseUrl/transfers'),
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+        body: data.toJson(),
+      );
+      print(res.body);
+      if (res.statusCode != 200) {
+        throw jsonDecode(res.body)['message'];
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> dataPlan(DataPlanFormModel data) async {
+    try {
+      print(data.toJson());
+      final token = await AuthService().getToken();
+      final res = await http.post(
+        Uri.parse('$baseUrl/data_plans'),
         headers: {
           'Authorization': 'Bearer $token',
         },
