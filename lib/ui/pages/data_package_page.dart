0395eae3d1c6cb5b98cfed8cf9cb32972a7c3d57
jesronmarudut23@gmail.com <jesronmarudut23@gmail.com>
2023-02-20 18:54:57 +0700

    Topup paket data jadi dinamis

--- lib/ui/pages/data_package_page.dart
+++ lib/ui/pages/data_package_page.dart
@@ -1,11 +1,15 @@
-import 'package:bank/blocs/operator_card/operator_card_bloc.dart';
+import 'package:bank/blocs/auth/auth_bloc.dart';
+import 'package:bank/blocs/data_plan/data_plan_bloc.dart';
+import 'package:bank/models/data_plan_form_model.dart';
 import 'package:bank/models/data_plan_model.dart';
 import 'package:bank/models/operator_card_model.dart';
+import 'package:bank/shared/shared_methods.dart';
 import 'package:bank/shared/theme.dart';
 import 'package:bank/ui/widgets/buttons.dart';
 import 'package:bank/ui/widgets/forms.dart';
 import 'package:bank/ui/widgets/package_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DataPackagePage extends StatefulWidget {
   final OperatorCardModel operatorCard;
@@ -25,64 +29,110 @@ class _DataPackagePageState extends State<DataPackagePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Paket Data'),
-      ),
-      body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        children: [
-          const SizedBox(height: 30),
-          Text(
-            'Phone Number',
-            style: whiteTextStyle.copyWith(fontSize: 16, fontWeight: semiBold),
-          ),
-          const SizedBox(height: 14),
-          CustomFormField(
-            keyboardType: TextInputType.number,
-            title: '+62 ',
-            isShowTitle: false,
-            controller: phoneController,
-          ),
-          const SizedBox(height: 40),
-          Text(
-            'Select Package',
-            style: whiteTextStyle.copyWith(
-              fontSize: 16,
-              fontWeight: semiBold,
-            ),
-          ),
-          const SizedBox(height: 14),
-          Wrap(
-            spacing: 17,
-            runSpacing: 17,
-            children: widget.operatorCard.dataPlans!.map((dataPlan) {
-              return GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      selectedDataPlan = dataPlan;
-                    });
-                  },
-                  child: PackageItem(
-                    dataPlan: dataPlan,
-                    isSelected: dataPlan.id == selectedDataPlan?.id,
-                  ));
-            }).toList(),
-          ),
-          const SizedBox(height: 57),
-        ],
-      ),
-      floatingActionButton:
-          (selectedDataPlan != null && phoneController.text.isNotEmpty)
-              ? Container(
-                  margin: const EdgeInsets.all(24),
-                  child: CustomFilledButton(
-                    title: 'Continue',
-                    onPressed: () {},
+    return BlocProvider(
+      create: (context) => DataPlanBloc(),
+      child: BlocConsumer<DataPlanBloc, DataPlanState>(
+        listener: (context, state) {
+          if (state is DataPlanFailed) {
+            showCustomSnackbar(context, state.e);
+          }
+          if (state is DataPlanSuccess) {
+            context.read<AuthBloc>().add(
+                  AuthUpdateBalance(
+                    selectedDataPlan!.price! * -1,
                   ),
-                )
-              : Container(),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+                );
+            Navigator.pushNamedAndRemoveUntil(
+                context, '/data-success', (route) => false);
+          }
+        },
+        builder: (context, state) {
+          if (state is DataPlanLoading) {
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          }
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Paket Data'),
+            ),
+            body: ListView(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              children: [
+                const SizedBox(height: 30),
+                Text(
+                  'Phone Number',
+                  style: whiteTextStyle.copyWith(
+                      fontSize: 16, fontWeight: semiBold),
+                ),
+                const SizedBox(height: 14),
+                CustomFormField(
+                  keyboardType: TextInputType.number,
+                  title: '+62 ',
+                  isShowTitle: false,
+                  controller: phoneController,
+                ),
+                const SizedBox(height: 40),
+                Text(
+                  'Select Package',
+                  style: whiteTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: semiBold,
+                  ),
+                ),
+                const SizedBox(height: 14),
+                Wrap(
+                  spacing: 17,
+                  runSpacing: 17,
+                  children: widget.operatorCard.dataPlans!.map((dataPlan) {
+                    return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            selectedDataPlan = dataPlan;
+                          });
+                        },
+                        child: PackageItem(
+                          dataPlan: dataPlan,
+                          isSelected: dataPlan.id == selectedDataPlan?.id,
+                        ));
+                  }).toList(),
+                ),
+                const SizedBox(height: 57),
+              ],
+            ),
+            floatingActionButton: (selectedDataPlan != null &&
+                    phoneController.text.isNotEmpty)
+                ? Container(
+                    margin: const EdgeInsets.all(24),
+                    child: CustomFilledButton(
+                      title: 'Continue',
+                      onPressed: () async {
+                        if (await Navigator.pushNamed(context, '/pin') ==
+                            true) {
+                          final authState = context.read<AuthBloc>().state;
+                          String pin = '';
+                          if (authState is AuthSuccess) {
+                            pin = authState.user.pin!;
+                          }
+                          context.read<DataPlanBloc>().add(
+                                DataPlanPost(
+                                  DataPlanFormModel(
+                                    dataPlanId: selectedDataPlan!.id.toString(),
+                                    phoneNumber: phoneController.text,
+                                    pin: pin,
+                                  ),
+                                ),
+                              );
+                        }
+                      },
+                    ),
+                  )
+                : Container(),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+          );
+        },
+      ),
     );
   }
 }
